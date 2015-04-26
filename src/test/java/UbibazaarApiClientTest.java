9b9d99f477ccf565ea6e1417fd19131d8c1b60b7
Simon Stastny <stastny.simon@gmail.com>
2015-04-26 09:38:39 +0200

    server address goes to properties

--- src/test/java/UbibazaarApiClientTest.java
+++ src/test/java/UbibazaarApiClientTest.java
@@ -1,5 +1,8 @@
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
@@ -7,6 +10,7 @@ import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,15 +19,25 @@ import org.ubicollab.ubibazaar.core.Platform;
 import com.google.gson.Gson;
 
 public class UbibazaarApiClientTest {
-
-  private static final String SERVER_ADDRESS = "http://10.0.0.114:8080/ubibazaar-api";
+  
+  private static String serverAddress;
+  
+  @BeforeClass
+  public static void setUp() throws IOException {
+    Properties prop = new Properties();
+    InputStream in = UbibazaarApiClientTest.class.getResourceAsStream("api.properties");
+    prop.load(in);
+    in.close();
+    
+    serverAddress = prop.getProperty("server_address");
+  }
 
   Logger logger = LoggerFactory.getLogger(getClass());
   
   @Test
   public void testPing() {
     WebTarget target = ClientBuilder.newClient()
-        .target(SERVER_ADDRESS)
+        .target(serverAddress)
         .path("/resources/ping");
 
     Map<String,Long> response = target
@@ -38,7 +52,7 @@ public class UbibazaarApiClientTest {
   @Test
   public void testPlatforms() {
     WebTarget target = ClientBuilder.newClient()
-        .target(SERVER_ADDRESS)
+        .target(serverAddress)
         .path("/resources/platform");
 
     List<Platform> platforms = target
