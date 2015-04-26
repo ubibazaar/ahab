e696ef68f6ca0d508c3bb4ec508490f990e6d13b
Simon Stastny <stastny.simon@gmail.com>
2015-04-26 08:53:55 +0200

    implementing test against the new entities

--- src/test/java/UbibazaarApiClientTest.java
+++ src/test/java/UbibazaarApiClientTest.java
@@ -1,27 +1,52 @@
 import java.util.List;
+import java.util.Map;
 
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 
+import org.junit.Assert;
 import org.junit.Test;
-import org.ubicollab.ubibazaar.core.Instance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.ubicollab.ubibazaar.core.Platform;
+
+import com.google.gson.Gson;
 
 public class UbibazaarApiClientTest {
 
+  private static final String SERVER_ADDRESS = "http://10.0.0.114:8080/ubibazaar-api";
+
+  Logger logger = LoggerFactory.getLogger(getClass());
+  
   @Test
-  public void testInstances() {
-    WebTarget target =
-        ClientBuilder.newClient().target("http://10.0.0.114:8080")
-            .path("ubibazaar-api/resources/instance");
+  public void testPing() {
+    WebTarget target = ClientBuilder.newClient()
+        .target(SERVER_ADDRESS)
+        .path("/resources/ping");
 
-    List<Instance> instances =
-        target.request(MediaType.APPLICATION_JSON_TYPE).get(new GenericType<List<Instance>>() {});
+    Map<String,Long> response = target
+        .request(MediaType.APPLICATION_JSON_TYPE)
+        .get(new GenericType<Map<String,Long>>() {});
+    
+    Assert.assertNotNull(response);
+    
+    logger.info("Ping response: " + new Gson().toJson(response));
+  }
+  
+  @Test
+  public void testPlatforms() {
+    WebTarget target = ClientBuilder.newClient()
+        .target(SERVER_ADDRESS)
+        .path("/resources/platform");
 
-    for (Instance instance : instances) {
-      System.out.println(instance.getApp().getName() + " on " + instance.getDevice().getName()
-          + instance.getApp().getExtra());
-    }
+    List<Platform> platforms = target
+        .request(MediaType.APPLICATION_JSON_TYPE)
+        .get(new GenericType<List<Platform>>() {});
+    
+    Assert.assertTrue(!platforms.isEmpty());
+    
+    logger.info("Supported platforms: " + new Gson().toJson(platforms));
   }
 }
